3ab908a065adcd1cca536aa4c3b054b4223bd53c
André Aubin <andre.aubin@lambdaweb.fr>
2014-01-28 19:47:41 +0100

    Adding basic static files for Morales & corentin

--- client/views/manage.js
+++ client/views/manage.js
@@ -1,8 +1,45 @@
 
+Questions = new Meteor.Collection("questions");
 
 Template.manage.helpers(
 {
   test_variable: function() {
     return 'Ceci est une variable de test qui est envoyee dans le template "manage"'
+  },
+  questions: function()
+  {
+  	var q = Questions.find().fetch();
+  	if (q.length == 0)
+  	{
+  		//console.log("ya rien dans la bdd !");
+  		// var questions = 
+		// [
+		// 	{
+		// 		question : "Qui est l'auteur(e) de James Bond ?",
+		// 		reponses : [
+		// 			{reponse : "Ian Fleming", valid : true},
+		// 			{reponse : "Arthur Conan Doyle", valid : false},
+		// 			{reponse : "Mary Shelley", valid : false},
+		// 			{reponse : "Marc LŽvy", valid : false},
+		// 		],
+		// 		categorie : "Culture Generale"
+		// 	}
+		// ]
+		// Questions.insert({
+		// 		question : "Qui est l'auteur(e) de James Bond ?",
+		// 		reponses : [
+		// 			{reponse : "Ian Fleming", valid : true},
+		// 			{reponse : "Arthur Conan Doyle", valid : false},
+		// 			{reponse : "Mary Shelley", valid : false},
+		// 			{reponse : "Marc LŽvy", valid : false},
+		// 		],
+		// 		categorie : "Culture Generale"
+		// 	});
+  	}
+  	else
+  	{
+  		console.log("ya de trucs");
+  	}
+  	return (Questions.find().fetch());
   }
 });

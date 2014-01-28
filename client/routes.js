67cd7086c43b5216c8ca4c954cea739cb4178510
André Aubin <andre.aubin@lambdaweb.fr>
2014-01-28 17:20:57 +0100

    Adding manager controller & template

--- client/routes.js
+++ client/routes.js
@@ -2,10 +2,19 @@ Router.configure({
   layout: 'layout'
 });
 
-Router.map(function() {
+Router.map(function()
+{
+
   this.route('home', {
     path: '/',
     template: 'home'
   });
+
+  // The manager URL
+  this.route('manage', {
+    path: '/manage',
+    template: 'manage'
+  });
+
 });
 
---
+++ client/views/manage.js
@@ -0,0 +1,8 @@
+
+
+Template.manage.helpers(
+{
+  test_variable: function() {
+    return 'Ceci est une variable de test qui est envoyee dans le template "manage"'
+  }
+});
